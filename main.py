e3b4690880c6f2455f50e9300be399e87308b3bc
SerPan23 <igracom@yandex.ru>
2020-09-05 23:40:43 +0300

    u

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import db
 import kb
 import settings as se
 import telebot
-import func as f
 bot = telebot.TeleBot(se.TOKEN)
 
 
@@ -15,13 +14,13 @@ def send_welcome(message):
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
     if call.data == "timetableDay":
-        f.send_day_lessons(call.message)
+        send_day_lessons(call.message)
     elif call.data == "timetableWeek":
-        f.send_week_lessons(call.message)
+        send_week_lessons(call.message)
     elif call.data == "homeworkDay":
         # f.send_day_hw(call.message)
         m = bot.send_message(call.message.chat.id, "Напишите номер дня.месяца:")
-        bot.register_next_step_handler(m, f.get_day_num)
+        bot.register_next_step_handler(m, get_day_num)
 
     elif call.data == "timetableBuffet":
         pass
@@ -35,6 +34,37 @@ def callback_worker(call):
 def callback_msg(message):
     if message.text == 'Назад':
         send_welcome(message)
+
+
+def get_day_num(message):
+    dn = message.text
+    if len(dn) == 5:
+        f = dn[0]+dn[1]
+        t = dn[2]
+        s = dn[3]+dn[4]
+        if (int(f) >= 1 or int(f) <= 31) and t == "." and (int(s) >= 1 or int(s) <= 12):
+            print('yes')
+            send_day_hw(message, dn)
+        else:
+            print('no1')
+            m = bot.send_message(message.chat.id, "Напишите номер дня.месяца (в формате 00.00):")
+            bot.register_next_step_handler(m, get_day_num)
+    else:
+        print('no2')
+        m = bot.send_message(message.chat.id, "Напишите номер дня.месяца (в формате 00.00):")
+        bot.register_next_step_handler(m, get_day_num)
+
+
+def send_day_lessons(message):
+    bot.send_message(message.chat.id, "Расписание:", reply_markup=kb.BackKb)
+
+
+def send_week_lessons(message):
+    bot.send_message(message.chat.id, "Расписание на неделю:", reply_markup=kb.BackKb)
+
+
+def send_day_hw(message, dn):
+    bot.send_message(message.chat.id, "Домашка: " + dn, reply_markup=kb.BackKb)
 
 
 bot.enable_save_next_step_handlers(delay=2)
