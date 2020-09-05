ff367495c9585217822ac8f98a89c2431d255316
mnklop <maxim210204@yandex.ru>
2020-09-05 22:49:13 +0300

    1234

--- db.py
+++ db.py
@@ -1,22 +1,17 @@
 import json
 import datetime
-import db_setup
+
 with open("data_file.json", "r") as read_file:
     data = json.load(read_file)
+
 
 def send_day_lessons():
     return data["student_lessons"][f"{datetime.date.today()}"]['lessons']
 
 
 def send_week_lessons():
-    return data["student_lessons"]["all_days"]
+    return data["all_days"]
 
 
-def send_day_hw():
-    pass
-
-
-def send_week_hw():
-    pass
-
-print(send_week_lessons())
+def send_day_hw(day):
+    return data["student_lessons"][f"{day}"]["HW"]
--- db_setup.py
+++ db_setup.py
@@ -4,51 +4,71 @@ data = {
     "student_lessons": {
         "2020-09-05": {
             "HW": {
-                    "lesson1": "балдеж",
-                    "lesson2": "балдеж2"
+                "lesson1": "балдеж",
+                "lesson2": "балдеж2"
             },
             "lessons":
-            {
-                "math": "9:00",
-                "english": "10:00"
-            }
+                {
+                    "math": "9:00",
+                    "english": "10:00"
+                }
         },
         "2020-09-06": {
             "HW": {
-                    "lesson1": "балдеж",
-                    "lesson2": "балдеж2"
+                "lesson1": "балдеж",
+                "lesson2": "балдеж2"
             },
             "lessons":
+                {
+                    "math": "9:00",
+                    "english": "10:00"
+                }
+        }
+
+    },
+    "all_days": {
+        "0":
             {
                 "math": "9:00",
                 "english": "10:00"
+
+            },
+        "1":
+            {
+                "math": "9:00",
+                "english": "10:00"
+
+            },
+        "2":
+            {
+                "math": "9:00",
+                "english": "10:00"
+
+            },
+        "3":
+            {
+                "math": "9:00",
+                "english": "10:00"
+
+            },
+        "4":
+            {
+                "math": "9:00",
+                "english": "10:00"
+
             }
-        },
-        "all_days":{
-            "monday":
-                {
-                    "math": "9:00",
-                    "english": "10:00"
-
-                },
-            "thuesday":
-                {
-                    "math": "9:00",
-                    "english": "10:00"
-
-                },
-            "wensday":
-                {
-                    "math": "9:00",
-                    "english": "10:00"
-
-                }
-
-
-        }
 
     }
 }
 parsed_string = json.dumps(data)
 with open("data_file.json", "w") as write_file:
     json.dump(data, write_file)
+
+
+def add_day(date, day):
+    global data
+    data["student_lessons"] = {
+        f"{date}": {"HW": {}, "lesson": {f"{data['all_days'][f'{day}']}"}}}
+
+
+add_day("2020-09-07", 3)
