1fab6d68c3af26476eb67ff7963c5db5096bb8d3
SerPan23 <igracom@yandex.ru>
2020-09-06 14:10:31 +0300

    u

--- db_setup.py
+++ db_setup.py
@@ -1,6 +1,6 @@
 import json
 
-data = {
+allDays = {
     "0":
         {
             "math": "9:00",
@@ -35,58 +35,21 @@ data = {
 }
 
 
-#         "2020-09-06": {
-#             "HW": {
-#                 "lesson1": "балдеж",
-#                 "lesson2": "балдеж2"
-#             },
-#             "lessons":
-#                 {
-#                     "math": "9:00",
-#                     "english": "10:00"
-#                 }
-#         }
-#
-#     },
-#     "all_days": {
-#         "0":
-#             {
-#                 "math": "9:00",
-#                 "english": "10:00"
-#
-#             },
-#         "1":
-#             {
-#                 "math": "9:00",
-#                 "english": "10:00"
-#
-#             },
-#         "2":
-#             {
-#                 "math": "9:00",
-#                 "english": "10:00"
-#
-#             },
-#         "3":
-#             {
-#                 "math": "9:00",
-#                 "english": "10:00"
-#
-#             },
-#         "4":
-#             {
-#                 "math": "9:00",
-#                 "english": "10:00"
-#
-#             }
-#
-#     }
-# }
-
+        # "2020-09-06": {
+        #     "HW": {
+        #         "lesson1": "балдеж",
+        #         "lesson2": "балдеж2"
+        #     },
+        #     "lessons":
+        #         {
+        #             "math": "9:00",
+        #             "english": "10:00"
+        #         }
+        # }
 
 def add_day(date, day):
-    global data
-    k = {date: {"HW": {}, "lesson": f"{data.get(f'{day}')}"}}
+    global allDays
+    k = {date: {"HW": {}, "lesson": f"{allDays.get(f'{day}')}"}}
     with open("student_lessons.json", "a") as write_file:
         json.dump(k, write_file)
 
