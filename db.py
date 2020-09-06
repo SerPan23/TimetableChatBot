fd77c3b1838f76c92eaf739c0026f8379158209b
SerPan23 <igracom@yandex.ru>
2020-09-06 21:41:26 +0300

    u

--- db.py
+++ db.py
@@ -1,26 +1,21 @@
-import json
 import datetime
 import settings as se
-import pymongo as pymongo
+import pymongo
 
 import db_setup as dbs
 
 mdb = pymongo.MongoClient(se.MONGODB_LINK)[se.MONGO_DB]
+print(mdb)
 
-with open("student_lessons.json", "r") as read_file:
-    data = json.load(read_file)
-
-
-def send_day_lessons():
-    return data[f"{datetime.date.today()}"]['lessons']
-
-
-def send_week_lessons():
-    return dbs.allDays
-
-
-def send_day_hw(day):
-    return data[f"{day}"]["HW"]
+# def send_day_lessons():
+#     return data[f"{datetime.date.today()}"]['lessons']
+#
+#
+# def send_week_lessons():
+#     return dbs.allDays
+#
+#
+# def send_day_hw(day):
+#     return data[f"{day}"]["HW"]
 
 
-print(send_day_hw())
