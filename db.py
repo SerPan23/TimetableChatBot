e7810bd577f9ee63002c9b8d33dccc73688c507f
mnklop <maxim210204@yandex.ru>
2020-09-07 17:40:14 +0300

    123455

--- db.py
+++ db.py
@@ -1,11 +1,14 @@
 import pymongo
 import settings as se
+import datetime
 
 mdb = pymongo.MongoClient(se.MONGODB_LINK)[se.MONGO_DB]
 
-
-def add_day():
-    pass
+def add_day(date, day):
+    allDay = mdb.all_days
+    days = {date: {"HW": {}, "lesson": {f"{allDay}": f'{day}'}}}
+    add = mdb.student_lesson
+    add.insert_one(days).inserted_id
 
 
 def add_hw():
@@ -25,42 +28,12 @@ def send_day_hw():
 
 
 def add_lesson():
-    allDays = {
-        "0":
-            {
-                "math": "9:00",
-                "english": "10:00"
+    day = {}
+    allDay = mdb.student_lesson
+    allDay.insert_one().inserted_id
 
-            },
-        "1":
-            {
-                "math": "9:00",
-                "english": "10:00"
 
-            },
-        "2":
-            {
-                "math": "9:00",
-                "english": "10:00"
-
-            },
-        "3":
-            {
-                "math": "9:00",
-                "english": "10:00"
-
-            },
-        "4":
-            {
-                "math": "9:00",
-                "english": "10:00"
-
-            }
-
-    }
-    allDay = mdb.all_days
-    allDay.insert_one(allDays).inserted_id
-add_lesson()
+add_day(datetime.date.today(), 3)
 # def give_lessons():
 #     l = []
 #     for i in mdb.lessons.find():
--- db_setup.py
+++ db_setup.py
@@ -1,59 +1,59 @@
-import json
-
-allDays = {
-    "0":
-        {
-            "math": "9:00",
-            "english": "10:00"
-
-        },
-    "1":
-        {
-            "math": "9:00",
-            "english": "10:00"
-
-        },
-    "2":
-        {
-            "math": "9:00",
-            "english": "10:00"
-
-        },
-    "3":
-        {
-            "math": "9:00",
-            "english": "10:00"
-
-        },
-    "4":
-        {
-            "math": "9:00",
-            "english": "10:00"
-
-        }
-
-}
-
-
-# "2020-09-06": {
-#     "HW": {
-#         "lesson1": "балдеж",
-#         "lesson2": "балдеж2"
-#     },
-#     "lessons":
+# import json
+#
+# allDays = {
+#     "0":
 #         {
 #             "math": "9:00",
 #             "english": "10:00"
+#
+#         },
+#     "1":
+#         {
+#             "math": "9:00",
+#             "english": "10:00"
+#
+#         },
+#     "2":
+#         {
+#             "math": "9:00",
+#             "english": "10:00"
+#
+#         },
+#     "3":
+#         {
+#             "math": "9:00",
+#             "english": "10:00"
+#
+#         },
+#     "4":
+#         {
+#             "math": "9:00",
+#             "english": "10:00"
+#
 #         }
+#
 # }
-
-def add_day(date, day):
-    global allDays
-    k = {date: {"HW": {}, "lesson": f"{allDays.get(f'{day}')}"},}
-    with open("student_lessons.json", "a") as write_file:
-        json.dump(k, write_file)
-
-
-add_day("2020-09-11", 4)
-# data["student_lessons"] = {
-#     date: {"HW": {}, "lesson": {f"{data['all_days'][f'{day}']}"}}}
+#
+#
+# # "2020-09-06": {
+# #     "HW": {
+# #         "lesson1": "балдеж",
+# #         "lesson2": "балдеж2"
+# #     },
+# #     "lessons":
+# #         {
+# #             "math": "9:00",
+# #             "english": "10:00"
+# #         }
+# # }
+#
+# def add_day(date, day):
+#     global allDays
+#     k = {date: {"HW": {}, "lesson": f"{allDays.get(f'{day}')}"},}
+#     with open("student_lessons.json", "a") as write_file:
+#         json.dump(k, write_file)
+#
+#
+# add_day("2020-09-11", 4)
+# # data["student_lessons"] = {
+# #     date: {"HW": {}, "lesson": {f"{data['all_days'][f'{day}']}"}}}
